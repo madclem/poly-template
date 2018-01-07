a6c0e27e4a0259d1a0937cca2e5085ed9b4ebda0
mad_clem <clem.chenebault@gmail.com>
2018-01-07 16:10:16 +0000

    run dev on windows

--- tools/webpack/dev.js
+++ tools/webpack/dev.js
@@ -4,24 +4,45 @@ const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseConfig = require('./base');
 const defaultSettings = require('./defaults');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const ip = require('ip').address();
+
 
 const config = Object.assign({}, baseConfig, {
+  debug: true,
+
   entry: [
-    'webpack-dev-server/client?http://127.0.0.1:' + defaultSettings.port,
+    'babel-polyfill',
+    `webpack-dev-server/client?http://${ip}:${defaultSettings.port}`,
     'webpack/hot/dev-server',
     './src/js/index'
   ],
   cache: true,
+  devtool: 'eval',//inline-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'dist/index.html'
-  	}),
-    new OpenBrowserPlugin({ url: 'http://localhost:'+ defaultSettings.port })
+    }),
+
+    new OpenBrowserPlugin({ url: `http://${ip}:${defaultSettings.port}` })
   ],
-  module: defaultSettings.getDefaultModules()
+  module: defaultSettings.getDefaultModules(),
+  //  resolve: {
+    //  fallback: path.join(__dirname, "node_modules"),
+    //  'pixi.js': path.join(__dirname, 'node_modules/pixi.js')
+
+  //  },
+  resolveLoader: {
+    root: path.join(__dirname, '../../node_modules'),
+  },
+  eslint: {
+    failOnWarning: false,
+    failOnError: false
+  },
+
 
 });
 

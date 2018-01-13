65e137149d46a68669ab9141134c346a8dfa8d68
mad_clem <clem.chenebault@gmail.com>
2018-01-13 15:13:40 +0000

    example multi objects being rendered

--- src/js/scenes/MainScene.js
+++ src/js/scenes/MainScene.js
@@ -72,141 +72,19 @@ export default class MainScene
 		let state = new POLY.State(this.gl);
 		state.blend = true;
 		state.blendMode = true;
+		
 	    this.program = new POLY.Program(vert, frag, uniforms);
-	    this.cube = new POLY.Mesh(this.program, state);
 
-		// this.cube.position.x = .5;
+	    this.cubes = [];
 
-	    const vertices = [
-	        // Front face
-	        -1.0, -1.0,  1.0,
-	         1.0, -1.0,  1.0,
-	         1.0,  1.0,  1.0,
-	        -1.0,  1.0,  1.0,
-
-	        // Back face
-	        -1.0, -1.0, -1.0,
-	        -1.0,  1.0, -1.0,
-	         1.0,  1.0, -1.0,
-	         1.0, -1.0, -1.0,
-
-	        // Top face
-	        -1.0,  1.0, -1.0,
-	        -1.0,  1.0,  1.0,
-	         1.0,  1.0,  1.0,
-	         1.0,  1.0, -1.0,
-
-	        // Bottom face
-	        -1.0, -1.0, -1.0,
-	         1.0, -1.0, -1.0,
-	         1.0, -1.0,  1.0,
-	        -1.0, -1.0,  1.0,
-
-	        // Right face
-	         1.0, -1.0, -1.0,
-	         1.0,  1.0, -1.0,
-	         1.0,  1.0,  1.0,
-	         1.0, -1.0,  1.0,
-
-	        // Left face
-	        -1.0, -1.0, -1.0,
-	        -1.0, -1.0,  1.0,
-	        -1.0,  1.0,  1.0,
-	        -1.0,  1.0, -1.0
-	    ];
-
-	     var textureCoords = [
-	      // Front face
-	      0.0, 0.0,
-	      1.0, 0.0,
-	      1.0, 1.0,
-	      0.0, 1.0,
-
-	      // Back face
-	      1.0, 0.0,
-	      1.0, 1.0,
-	      0.0, 1.0,
-	      0.0, 0.0,
-
-	      // Top face
-	      0.0, 1.0,
-	      0.0, 0.0,
-	      1.0, 0.0,
-	      1.0, 1.0,
-
-	      // Bottom face
-	      1.0, 1.0,
-	      0.0, 1.0,
-	      0.0, 0.0,
-	      1.0, 0.0,
-
-	      // Right face
-	      1.0, 0.0,
-	      1.0, 1.0,
-	      0.0, 1.0,
-	      0.0, 0.0,
-
-	      // Left face
-	      0.0, 0.0,
-	      1.0, 0.0,
-	      1.0, 1.0,
-	      0.0, 1.0,
-	    ];
-
-	    var vertexNormals = [
-			// Front face
-			0.0,  0.0,  1.0,
-			0.0,  0.0,  1.0,
-			0.0,  0.0,  1.0,
-			0.0,  0.0,  1.0,
-
-			// Back face
-			0.0,  0.0, -1.0,
-			0.0,  0.0, -1.0,
-			0.0,  0.0, -1.0,
-			0.0,  0.0, -1.0,
-
-			// Top face
-			0.0,  1.0,  0.0,
-			0.0,  1.0,  0.0,
-			0.0,  1.0,  0.0,
-			0.0,  1.0,  0.0,
-
-			// Bottom face
-			0.0, -1.0,  0.0,
-			0.0, -1.0,  0.0,
-			0.0, -1.0,  0.0,
-			0.0, -1.0,  0.0,
-
-			// Right face
-			1.0,  0.0,  0.0,
-			1.0,  0.0,  0.0,
-			1.0,  0.0,  0.0,
-			1.0,  0.0,  0.0,
-
-			// Left face
-			-1.0,  0.0,  0.0,
-			-1.0,  0.0,  0.0,
-			-1.0,  0.0,  0.0,
-			-1.0,  0.0,  0.0,
-		];
-
-	    var cubeVertexIndices = [
-	        0, 1, 2,      0, 2, 3,    // Front face
-	        4, 5, 6,      4, 6, 7,    // Back face
-	        8, 9, 10,     8, 10, 11,  // Top face
-	        12, 13, 14,   12, 14, 15, // Bottom face
-	        16, 17, 18,   16, 18, 19, // Right face
-	        20, 21, 22,   20, 22, 23  // Left face
-	    ];
-
-
-	    this.cube.addPosition(vertices);
-	    this.cube.addAttribute(textureCoords, 'aUv', 2);
-	    this.cube.addAttribute(vertexNormals, 'aNormal', 3);
-	    this.cube.addIndices(cubeVertexIndices, false);
-
-	    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	    for (var i = 0; i < 100; i++) {
+	    	let cube = new POLY.geometry.Cube(this.program, state, {});
+	    	cube.position.x = Math.random() * 4 - 4/2;
+	    	cube.position.y = Math.random() * 4 - 4/2;
+	    	cube.position.z = Math.random() * 4 - 4/2;
+	    	this.cubes.push(cube);
+	    }
+    	// this.cube = new POLY.geometry.Cube(this.program, state, {});
 	}
 
 	render()
@@ -215,26 +93,26 @@ export default class MainScene
 
 		this.orbitalControl.update();
 		this.camera.position[2] += .01;
-		// this.cube.rotation.y += .1;
-		this.cube.setScale(1 + Math.cos(this.tick/20) * .5, 1 + Math.sin(this.tick/20) * .5, 1 + Math.cos(this.tick/20) * .5);
-
-		// normal matrix
-		mat4.multiply(this._matrix, this.camera.matrix, this.cube._matrix);
-    	mat3.fromMat4(this.normalMatrix, this._matrix);
-    	mat3.transpose(this.normalMatrix, this.normalMatrix);
-
 	    this.program.uniforms.uTexture.bind();
 
 		// set uniforms
-	    this.program.uniforms.modelMatrix = this.cube._matrix;
 	    this.program.uniforms.projectionMatrix = this.camera.projectionMatrix;
 	    this.program.uniforms.viewMatrix = this.camera.matrix;
 		this.program.uniforms.normalMatrix = this.normalMatrix;
-		// this.program.uniforms.uAmbientColor = [.2, .2, .2];
-		// this.program.uniforms.uLightingDirection = [.25, .25, 1.];
-		// this.program.uniforms.uDirectionalColor = [.8, .8,.8];
 
-	    POLY.GL.draw(this.cube);
+		for (var i = 0; i < this.cubes.length; i++) 
+		{
+			let c = this.cubes[i];
+			c.rotation.y += .05;
+			
+			// normal matrix
+			mat4.multiply(this._matrix, this.camera.matrix, c._matrix);
+    		mat3.fromMat4(this.normalMatrix, this._matrix);
+    		mat3.transpose(this.normalMatrix, this.normalMatrix);
+
+			this.program.uniforms.modelMatrix = c._matrix;
+	    	POLY.GL.draw(c);
+		}
 	}
 
 	resize()
